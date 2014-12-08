19e370a775d0bcc8da34fecfc944d2640b260eb1
ryanmuller <cognitionmachine@gmail.com>
2014-12-07 21:55:11 -0500

    ???

--- static/js/app.js
+++ static/js/app.js
@@ -8,20 +8,29 @@ $(document).on("click", ".list.btn", function() {
 
 $(document).on("click", ".check.btn", function() {
   var lightbox_id = $(this).data("lightbox-id");
-  $.post("/lightboxes/"+lightbox_id+"/answers",
-         { "text": $(".text").val() },
-         function(data) {
-           clearInterval(checkInterv);
-           answerId = data.answer_id;
-           checkInterv = window.setInterval(function() {
-             $.get("/answers/"+answerId+"/result", function(data) {
-               console.log(data);
-               if(data.hasOwnProperty("label")) {
-                 clearInterval(checkInterv);
-               }
-             });
-           }, 500);
-         });
+  $(".result span").text("Loading...");
+  $.post(
+    "/lightboxes/"+lightbox_id+"/answers",
+    { "text": $(".text").val() },
+    function(data) {
+      clearInterval(checkInterv);
+      answerId = data.answer_id;
+      checkInterv = window.setInterval(function() {
+        $.get("/answers/"+answerId+"/result", function(data) {
+          if(data.hasOwnProperty("label")) {
+            clearInterval(checkInterv);
+
+            if (data.label === "0" || data.label === "1") {
+              $(".result span").text("Hmm... try answering differently.");
+            } else if (data.label === "2" || data.label === "3") {
+              $(".result span").text("Good start! Try explaining your answer further.");
+            } else if (data.label === "5") {
+              $(".result span").text("Awesome answer!");
+            }
+          }
+        });
+      }, 500);
+    });
 });
 
 $(document).on("click", ".prompt.btn", function() {
@@ -29,8 +38,10 @@ $(document).on("click", ".prompt.btn", function() {
   $.get("/lightboxes/"+lightbox_id, function(data) {
     $("#list").hide();
     $("#write").show();
-    $(".prompt h1").text(data.prompt);
-    $(".check.btn").data("lightbox-id", lightbox_id);
+    $("#write .prompt h1").text(data.prompt);
+    $("#write .text").val("");
+    $("#write .result span").text("");
+    $("#write .check.btn").data("lightbox-id", lightbox_id);
   });
 });
 
